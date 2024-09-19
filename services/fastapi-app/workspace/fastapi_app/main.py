3b3b67f40b06a344157f39fa88916efb9afe9cdf
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2024-09-19 23:05:56 +0200

    Add example docker solution

---
+++ services/fastapi-app/workspace/fastapi_app/main.py
@@ -0,0 +1,19 @@
+"""
+Short description and source of base code.
+
+@author "Daniel Mizsak" <info@pythonvilag.hu>
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
